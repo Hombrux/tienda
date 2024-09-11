01d683bded698a5999ea7edfd7dd7987bcb9e96e
Mauricio Martinez <61915644+Hombrux@users.noreply.github.com>
2024-09-10 22:25:36 -0700

    Delete admin directory

--- admin/src/app/app.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes,RouterModule } from "@angular/router";
-import { ModuleWithProviders } from "@angular/core";
-
-const appRoute : Routes = [
-
-];
-
-export const appRoutingProviders: any [] = [];
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
